7afbdd3c9765336d813bf99d8a7406219d31d4d4
mnengwa <howardmnengwa@gmail.com>
2022-05-10 16:36:47 +0300

    task: initial commit 1. Create a basic / simple Flask project structure 2. Implement Jinja2 templates & macros 3. Created a request module to handle the fetch of movies from the    Movie Database API 4. Implemented a route that renders the movies we have fectched from the    API

---
+++ app/request.py
@@ -0,0 +1,41 @@
+from app import app
+from models import movie
+import urllib.request,json
+
+Movie = movie.Movie
+api_key = app.config['MOVIE_API_KEY']
+base_url = app.config['MOVIE_API_BASE_URL']
+
+def process_results(collection):
+    movie_results = []
+
+    for item in collection:
+        id = item.get('id')
+        overview = item.get('overview')
+        poster = item.get('poster_path')
+        title = item.get('original_title')
+        vote_count = item.get('vote_count')
+        vote_average = item.get('vote_average')
+
+        if poster:
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+
+            movie_results.append(movie_object)
+
+    return movie_results
+
+def get_movies(category):
+    get_movies_url = f"{base_url}/{category}?api_key={api_key}"
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results = None
+
+        print(get_movies_response);
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+            movie_results = process_results(movie_results_list)
+
+    return movie_results
---
+++ app/views.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from .request import get_movies
+from app import app
+
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    latest_movies = get_movies('upcoming')
+
+    popular_movies = get_movies('popular')
+
+    return render_template('index.html', latest_movies = latest_movies, popular_movies = popular_movies)
